a25bc2c159026a079ee9c0c1028102d9ec5e7021
Julio Henrique Santos <JulioHenrique31501234@gmail.com>
2022-08-03 20:50:31 -0300

    Send message to all peers

--- ui/Chat.gd
+++ ui/Chat.gd
@@ -1,5 +1,18 @@
 extends VBoxContainer
 
 
+const MESSAGE = preload("res://ui/Message.tscn")
+
+onready var List = $Scroll/List
+onready var MessageLine = $MessageLine
+
+
+remotesync func add_message(author, message):
+	var msg = MESSAGE.instance()
+	msg.text = "%s: %s" % [author, message]
+	List.add_child(msg)
+
+
 func _on_text_entered(new_text):
-	pass
+	MessageLine.text = ""
+	rpc("add_message", Network.player_name, new_text)
